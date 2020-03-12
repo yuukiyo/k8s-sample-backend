d7c243fd54b727046e40539e1149efc0e04da8fa
Yuki Yoshida <yshdyk11202000@gmail.com>
2020-03-12 16:37:55 +0900

    ddb削除

--- hello.py
+++ hello.py
@@ -7,18 +7,6 @@ import os
 import decimal
 from aws_xray_sdk.core import xray_recorder, patch_all
 
-# Helper class to convert a DynamoDB item to JSON.
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if abs(o) % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
-
-ddb = boto3.resource('dynamodb')
-table = ddb.Table('appmesh_sample_message')
 app = Flask(__name__)                             
 patch_all()
 
